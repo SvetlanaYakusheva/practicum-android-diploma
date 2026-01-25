476d493b5457c2823d324dabf73c8846bcdb35b0
Hesoyam-01 <ashot.m1985@gmail.com>
2026-01-25 19:30:03 +0300

    удалил converter

--- app/src/main/java/ru/practicum/android/diploma/data/db/converter/FavoriteVacancyDbConverter.kt
+++
@@ -1,9 +0,0 @@
-package ru.practicum.android.diploma.data.db.converter
-
-import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyEntity
-
-class FavoriteVacancyDbConverter {
-    fun map() {}
-
-    fun map(vacancy: FavoriteVacancyEntity) {}
-}
