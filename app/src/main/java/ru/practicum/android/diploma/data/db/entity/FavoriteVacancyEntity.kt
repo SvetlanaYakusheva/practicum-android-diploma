b43703c9e658d39007d97555674dd24e559400cd
Hesoyam-01 <ashot.m1985@gmail.com>
2026-01-25 20:54:00 +0300

    добавил columninfo для vacancyid

--- app/src/main/java/ru/practicum/android/diploma/data/db/entity/FavoriteVacancyEntity.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/entity/FavoriteVacancyEntity.kt
@@ -1,10 +1,11 @@
 package ru.practicum.android.diploma.data.db.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "favorite_vacancy_table")
 data class FavoriteVacancyEntity(
-    @PrimaryKey
+    @PrimaryKey @ColumnInfo(name = "vacancy_id")
     val vacancyId: String
 )
