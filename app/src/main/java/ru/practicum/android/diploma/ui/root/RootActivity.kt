4a7fb784b010e1f2c42eb74a6fe13e54c6a1d0cf
Ingward Berholz <i@iberholz.ru>
2026-01-25 23:08:26 +0500

    Эпик0: навигация #3: Созданы заглушки и фрагменты (Search, Filter, Location, Country, Region, Industry, VacancyDetails, Favorites, DevelopmentTeam Navigation graph, BottomNavigationMenu – только на экране поиска вакансий

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -1,21 +1,75 @@
 package ru.practicum.android.diploma.ui.root
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.core.view.updatePadding
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
 
 class RootActivity : AppCompatActivity() {
+
+    private val binding: ActivityRootBinding by lazy { ActivityRootBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+
+        enableEdgeToEdge()
+
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container))
+        { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updatePadding(top = systemBars.top, bottom = systemBars.bottom)
+            insets
+        }
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.API_ACCESS_TOKEN)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.search_fragment -> {
+                    changeBottomNavigationVisibility(true)
+                }
+
+                R.id.favorites_fragment -> {
+                    changeBottomNavigationVisibility(true)
+                }
+
+                R.id.development_team_fragment -> {
+                    changeBottomNavigationVisibility(true)
+                }
+
+                else -> {
+                    changeBottomNavigationVisibility(false)
+                }
+            }
+        }
+
     }
 
     private fun networkRequestExample(accessToken: String) {
         // ...
+
+    }
+
+
+    private fun changeBottomNavigationVisibility(isVisible: Boolean) {
+        binding.bottomNavigationView.isVisible = isVisible
+        binding.separator.isVisible = isVisible
     }
 
 }
