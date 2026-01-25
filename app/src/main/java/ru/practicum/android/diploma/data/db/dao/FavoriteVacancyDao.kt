46d1c4d48f8bf5741ea1a24e2bfb62e52907a791
Hesoyam-01 <ashot.m1985@gmail.com>
2026-01-25 21:44:40 +0300

    onConflict = OnConflictStrategy.IGNORE

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoriteVacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoriteVacancyDao.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma.data.db.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyEntity
 
@@ -11,6 +12,6 @@ interface FavoriteVacancyDao {
     @Query("SELECT * FROM favorite_vacancy_table")
     suspend fun getVacancies(): List<FavoriteVacancyEntity>
 
-    @Insert(entity = FavoriteVacancyEntity::class)
+    @Insert(entity = FavoriteVacancyEntity::class, onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertVacancy(vacancy: FavoriteVacancyEntity)
 }
