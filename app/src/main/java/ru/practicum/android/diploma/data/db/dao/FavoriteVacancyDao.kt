d24fd0977b155860dc9eb83aec3556eb80b737c2
Hesoyam-01 <ashot.m1985@gmail.com>
2026-01-25 20:41:15 +0300

    удалил converter

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoriteVacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoriteVacancyDao.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.data.db.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyEntity
@@ -10,4 +11,7 @@ interface FavoriteVacancyDao {
 
     @Query("SELECT * FROM favorite_vacancy_table ORDER BY vacancyId DESC")
     fun getVacancies(): Flow<List<FavoriteVacancyEntity>>
+
+    @Insert(entity = FavoriteVacancyEntity::class)
+    suspend fun insertVacancy(vacancy: FavoriteVacancyEntity)
 }
