b5c431b75ce6aee4b5de929e1d50a9ef7d442b01
Hesoyam-01 <ashot.m1985@gmail.com>
2026-01-26 12:24:49 +0300

    import
    name
    
    /

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -2,22 +2,15 @@ package ru.practicum.android.diploma.di
 
 import androidx.room.Room
 import com.google.gson.Gson
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import ru.practicum.android.diploma.data.db.AppDatabase
-import ru.practicum.android.diploma.data.network.VacanciesAPI
 import ru.practicum.android.diploma.data.network.NetworkClient
 import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
-
-object Key {
-    const val BASE_URL = "https://practicum-diploma-8bc38133faba.herokuapp.com/"
-}
+import ru.practicum.android.diploma.data.network.VacanciesAPI
 
 val dataModule = module {
-
-    single { androidContext() }
 
     single<VacanciesAPI> {
         Retrofit.Builder()
@@ -38,4 +31,8 @@ val dataModule = module {
             .build()
     }
 
+}
+
+object Key {
+    const val BASE_URL = "https://practicum-diploma-8bc38133faba.herokuapp.com/"
 }
