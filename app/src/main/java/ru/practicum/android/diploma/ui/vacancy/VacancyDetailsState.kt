e74877fcbc7d5890088d9360277d28462cb47d7f
Ingward Berholz <i@iberholz.ru>
2026-01-30 22:03:39 +0500

    TASK-2.2-UI-State-экрана-деталей

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyDetailsState.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyDetailsState.kt
@@ -5,9 +5,9 @@ import ru.practicum.android.diploma.domain.models.Vacancy
 sealed interface VacancyDetailsState {
     data object Loading: VacancyDetailsState
 
-    data class Content(val vacancy: Vacancy) : VacancyDetailsState
+    data class Content(val vacancy: Vacancy): VacancyDetailsState
 
-    data object VacancyNotFoundError : VacancyDetailsState
+    data object VacancyNotFoundError: VacancyDetailsState
 
-    data object VacancyServerError : VacancyDetailsState
+    data object VacancyServerError: VacancyDetailsState
 }
