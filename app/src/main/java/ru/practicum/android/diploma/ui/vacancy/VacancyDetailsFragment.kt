b7605631c1096073d7625f38100e29838dd9ada2
Svetlana Yakusheva <onetwoquestion@ya.ru>
2026-01-30 11:20:11 +0300

    6 добавила E2-TASK-1.1 Навигация и открытие экрана: переход в графе навигации createArgs для передачи id вакансии

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyDetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import ru.practicum.android.diploma.databinding.FragmentVacancyDetailsBinding
 
@@ -12,13 +13,27 @@ class VacancyDetailsFragment : Fragment() {
     private val binding
         get() = _binding!!
 
+    var vacancyId: String? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentVacancyDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        vacancyId = requireArguments().getString(KEY_VACANCY_ID)
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+
+        const val KEY_VACANCY_ID = "KEY_VACANCY_ID"
+        fun createArgs(id: String): Bundle =
+            bundleOf(KEY_VACANCY_ID to id)
     }
 }
