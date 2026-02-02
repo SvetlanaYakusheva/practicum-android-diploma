7075f1c4eaa612874bddebd9a5f044107ad697ab
Svetlana Yakusheva <onetwoquestion@ya.ru>
2026-02-02 12:29:49 +0300

    -обработка нажатия кнопки Back -логотип компании сделала, как в item-view -поменяла обрезку  логотипа на fitCenter в vacancy_item -поменяла порядок импортов

--- app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/InteractorModule.kt
@@ -2,8 +2,8 @@ package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
 import ru.practicum.android.diploma.domain.api.SearchVacanciesInteractor
-import ru.practicum.android.diploma.domain.impl.SearchVacanciesInteractorImpl
 import ru.practicum.android.diploma.domain.api.VacancyDetailsInteractor
+import ru.practicum.android.diploma.domain.impl.SearchVacanciesInteractorImpl
 import ru.practicum.android.diploma.domain.impl.VacancyDetailsInteractorImpl
 
 val interactorModule = module {
