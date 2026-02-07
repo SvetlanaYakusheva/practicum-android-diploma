8357f4e84258b7e2e579f7e32f702a74dea94f5d
Hesoyam-01 <ashot.m1985@gmail.com>
2026-02-07 23:34:38 +0300

    fix

--- app/src/main/java/ru/practicum/android/diploma/data/impl/FavoriteVacanciesRepositoryImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/impl/FavoriteVacanciesRepositoryImpl.kt
@@ -9,6 +9,7 @@ import ru.practicum.android.diploma.domain.api.FavoriteVacanciesRepository
 import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.util.ErrorType
 import ru.practicum.android.diploma.util.Resource
+import kotlin.coroutines.cancellation.CancellationException
 
 class FavoriteVacanciesRepositoryImpl(
     private val appDatabase: AppDatabase,
@@ -23,8 +24,8 @@ class FavoriteVacanciesRepositoryImpl(
         emit(Resource.Success(vacancies))
 
     }.catch { e ->
-        // Ловим только реальные ошибки, не мешая корутинам отменяться
-        emit(Resource.Error(ErrorType.SQLError))
+        if (e is CancellationException) throw e
+        else emit(Resource.Error(ErrorType.SQLError))
 
     }
 
